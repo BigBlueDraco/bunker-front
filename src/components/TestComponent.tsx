88c04a624d7fc7e54ae7b9b4cf5a8500cfeb2a5a
IvanyukAndrew <ivanyuk.andrei@gmail.com>
2023-06-15 17:58:19 +0300

    initial commit v2

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppWrap = styled.div`
+  width: 150px;
+  margin: 0 auto;
+`;
+
+function TestComponent() {
+  return <AppWrap>Bunker Menu Front</AppWrap>;
+}
+
+export default TestComponent;
